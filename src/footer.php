1e7542c3c70b691d66e6d5fbf08cb1925d93d020
Tom Rouillon <tom.rouillon3@etu.univ-lorraine.fr>
2022-12-05 16:08:31 +0100

    mise au clair avec le code parce que Grégoire il sait pas faire

--- src/footer.php
+++ src/footer.php
@@ -1,19 +1,7 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="style.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>     
-        <footer>
-            <nav class="footer">
-                <p>
-                    <a href="contacts.php">Contact</a>
-                    <a href="aPropos.php">A propos</a>
-                </p>
-            </nav>
-        </footer>
-    </body>
-</html>
+   
+<footer>
+    <nav class="footer">
+        <a href="contacts.php">Contact</a>
+        <a href="aPropos.php">A propos</a>
+    </nav>
+</footer>
