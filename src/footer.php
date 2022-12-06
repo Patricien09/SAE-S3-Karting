d78af7be8693f8cf1a5505085f2bf62f335ea4f3
Wittmann Gregory <gregory.wittmann4@etu.univ-lorraine.fr>
2022-12-06 11:56:44 +0100

    add media

--- src/footer.php
+++ src/footer.php
@@ -4,6 +4,12 @@
         Copyright © 2022 | Designed by Tom Rouillon/Wittmann Gregory
     </p>
 
+    <p id="media">
+        <a href="https://www.facebook.com/"><img src="logos/Facebook.png" alt="facebook" width="50" height="50"></a>
+        <a href="https://www.instagram.com/"><img src="logos/Instagram.png" alt="instagram" width="50" height="50"></a>
+        <a href="https://www.youtube.com/"><img src="logos/Youtube.png" alt="youtube" width="50" height="50"></a>
+    </p>
+
     <nav class="footer">
         <a class="button" href="contacts.php">Contact</a>
         <a class="button" href="aPropos.php">A propos</a>
