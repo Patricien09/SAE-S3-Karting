01efcc9d96ee5737f4af0ca559d7634c838e8789
Wittmann Gregory <gregory.wittmann4@etu.univ-lorraine.fr>
2022-12-06 11:41:06 +0100

    Add links page

---
+++ src/contacts.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style/main.css" type="text/css">
+        <link rel="stylesheet" href="style/footer.css" type="text/css">
+        <link rel="stylesheet" href="style/header.css" type="text/css">
+        <title> Contacts </title>
+    </head>
+
+    <body>
+        <?php 
+            require_once("header.php"); 
+        ?>
+
+        <div id="contenu">
+            Bienvenu dans notre page de contacts
+        </div>
+
+        <?php 
+            require_once("footer.php");
+        ?>
+    </body>
+    
+</html>
