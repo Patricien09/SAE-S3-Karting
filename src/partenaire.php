10dc56af9554881374694aa4116f60b6ad1c214a
Wittmann Gregory <gregory.wittmann4@etu.univ-lorraine.fr>
2023-01-12 11:52:23 +0100

    Ajout de la page partenaire

--- src/partenaire.php
+++ src/partenaire.php
@@ -3,7 +3,38 @@
 ?>
 
 <div id="contenu">
-    Bienvenu dans notre où y'a nos partenaires
+        <div class="event-list"
+            <h1>Partenaires</h1>
+            <p>Voici la liste de nos partenaires</p>
+
+            <ul class="events">
+                <li class="event">Nom:<br>Fournisseur de karting</li>
+                <li class="event">Localisation:<br> Saint-Dié des Vosges</li>
+                <li class="event">Site:<br> <a href="">Fournisseur de karting</a></li>
+                <li class="event"> Descritpion:<br> Je suis un fournisseur de karting qui font vroum vroum </li>
+            </ul>
+
+            <ul class="events">
+                <li class="event">Nom:<br> Fournisseur de casques de protections</li>
+                <li class="event">Localisation:<br> Saint-Dié des Vosges</li>
+                <li class="event">Site:<br> <a href="">Fournisseur de casques de protection</a></li>
+                <li class="event"> Descritpion:<br> Je suis un fournisseur de casques de protection qui protègent </li>
+            </ul>
+
+            <ul class="events">
+                <li class="event">Nom:<br> Fournisseur de gants de protections</li>
+                <li class="event">Localisation:<br> Saint-Dié des Vosges</li>
+                <li class="event">Site:<br> <a href="">Fournisseur de gants de protection</a></li>
+                <li class="event"> Descritpion:<br> Je suis un fournisseur de gants de protection qui évitent de transformer ses mains en passoire </li>
+            </ul>
+
+            <ul class="events">
+                <li class="event">Nom:<br> Fournisseur de combinaisons de pilote</li>
+                <li class="event">Localisation:<br> Saint-Dié des Vosges</li>
+                <li class="event">Site:<br> <a href="">Fournisseur de combinaisons de pilote</a></li>
+                <li class="event"> Descritpion<br> : Je suis un fournisseur de combinaisons de pilote qui ont du swag </li>
+            </ul>
+    </div>
 </div>
 
 <?php 
